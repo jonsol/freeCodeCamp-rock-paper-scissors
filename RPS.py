dbc51dfae4feb8635cd5c86a3d3df4010d44dcae
adrian Bernabeu <adrianbernabeu@hotmail.com>
2025-01-27 16:49:30 +0000

    Firs

--- RPS.py
+++ RPS.py
@@ -3,8 +3,8 @@
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    guess = "P"
+    if len(opponent_history) > 5:
+        guess = opponent_history[-5]
 
     return guess
