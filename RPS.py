2a032ee5d9268a46fa59621b60940203f892b4d9
Namish Garimella <garimellanamish@gmail.com>
2025-09-26 01:29:02 +0530

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,20 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    
+    if prev_play != "":
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Default move if no history yet
+    if not opponent_history:
+        return "R"
 
-    return guess
+
+
+    freq = {"R": 0, "P": 0, "S": 0}
+    for move in opponent_history:
+        freq[move] += 1
+    predicted = max(freq, key=freq.get)
+
+    counter = {"R": "P", "P": "S", "S": "R"}
+    return counter[predicted]
